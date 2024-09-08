62f6cca3e2ce37ede860b966a1991ead13282b4d
Jamie Smith <jeasmith@me.com>
2024-09-08 13:55:55 +0000

    feat(lib): Add initial hmrc ta files and js supporting packages

---
+++ lib/tax/hmrc.test.ts
@@ -0,0 +1,30 @@
+import { getHmrcRates } from "./hmrc";
+
+describe("getHmrcRates", () => {
+  test("It uses default 2024/25 rates", () => {
+    const rates = getHmrcRates();
+    expect(rates.DEFAULT_PERSONAL_ALLOWANCE).toEqual(12570);
+    expect(rates.NI_MIDDLE_RATE).toEqual(0.08);
+  });
+
+  test("It uses explicit 2022/23 rates", () => {
+    expect(
+      getHmrcRates({ taxYear: "2022/23", country: "England/NI/Wales" })
+        .DEFAULT_PERSONAL_ALLOWANCE,
+    ).toEqual(12570);
+  });
+
+  test("It uses explicit 2023/24 rates", () => {
+    expect(
+      getHmrcRates({ taxYear: "2023/24", country: "England/NI/Wales" })
+        .DEFAULT_PERSONAL_ALLOWANCE,
+    ).toEqual(12570);
+  });
+
+  test("It uses explicit 2024/25 rates", () => {
+    expect(
+      getHmrcRates({ taxYear: "2024/25", country: "England/NI/Wales" })
+        .DEFAULT_PERSONAL_ALLOWANCE,
+    ).toEqual(12570);
+  });
+});
---
+++ lib/tax/index.ts
@@ -0,0 +1,5 @@
+export * from "./hmrc";
+export * from "./incomeTax";
+export * from "./nationalInsurance";
+export * from "./personalAllowance";
+export * from "./studentLoan";
